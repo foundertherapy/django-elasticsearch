9cc59f7fc6940592c78f8d1a8b40bc753d2b74cf
Nasief Khlaif <nasief304@gmail.com>
2018-10-08 08:59:33 +0300

    Update file to go with python 3

--- django_elasticsearch/utils.py
+++ django_elasticsearch/utils.py
@@ -2,7 +2,7 @@ import collections
 
 
 def nested_update(d, u):
-    for k, v in u.iteritems():
+    for k, v in u.items():
         if isinstance(v, collections.Mapping):
             r = nested_update(d.get(k, {}), v)
             d[k] = r
